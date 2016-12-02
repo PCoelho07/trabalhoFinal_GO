aa0518bc71e8ae1a193e4e7822ac3c6ebb33d972
PCoelho07 <pedro_coelho_pv@hotmail.com>
2016-12-02 01:11:48 -0300

    90% done, with any tests

--- main.go
+++ main.go
@@ -10,17 +10,17 @@ import (
 
 
 func aprontar(nome string) (bool, int) {
-	fmt.Println(nome + "está se aprontando")
-	t := random(60, 180)
-	time.Sleep(t * time.Seconds)
+	fmt.Println(nome + " está se aprontando")
+	t := random(6, 18)
+	time.Sleep(time.Duration(t) * time.Second)
 
 	return true, t
 }
 
 func calcar_tenis(nome string) (bool, int) {
-	fmt.Println(nome + "começou a calçar seu tênis")
-	t := random(30, 45)
-	time.Sleep(t * time.Seconds)
+	fmt.Println(nome + " começou a calçar seu tênis")
+	t := random(3, 4)
+	time.Sleep(time.Duration(t) * time.Second)
 
 	return true, t
 }
@@ -35,12 +35,12 @@ func ana(megafone_1 chan int) {
 	flag, t_a := aprontar("Ana")
 
 	if flag == true {
-		fmt.Println("Ana demorou " + t_a + " segundos para se preparar")
+		fmt.Println("Ana demorou",t_a,"segundos para se preparar")
 		megafone_1 <- 0
-		flag_1, t_ct := calçar_tenis("Ana")
+		flag_1, t_ct := calcar_tenis("Ana")
 
 		if flag_1 == true {
-			fmt.Println("Ana demorou " + t_ct + " segundos para calçar seu tenis")			
+			fmt.Println("Ana demorou",t_ct,"segundos para calçar seu tenis")			
 			megafone_1 <- 1		
 		}
 	}
@@ -51,33 +51,59 @@ func maria(megafone_2 chan int) {
 	flag, t_a := aprontar("Maria")
 
 	if flag == true {
-		fmt.Println("Maria demorou " + t_a + " segundos para se preparar")
+		fmt.Println("Maria demorou",t_a,"segundos para se preparar")
 		megafone_2 <- 0
-		flag_1, t_ct := calçar_tenis("Maria")
+		flag_1, t_ct := calcar_tenis("Maria")
 
 		if flag_1 == true {
-			fmt.Println("Maria demorou " + t_ct + " segundos para calçar seu tenis")			
+			fmt.Println("Maria demorou",t_ct,"segundos para calçar seu tenis")		
 			megafone_2 <- 1		
 		}
 	}
 }
 
-func gerenciador() {
-	
-}
 
 
-func alarme(megafone chan bool) {
-
-
-}
+// func alarme() {
+// 	fmt.Println("Alarme foi acionado")
+// 	fmt.Println("Alarme está em contagem regressiva")
+// 	timer := time.NewTimer(time.Second * 6)
+// 	<-timer.C
+// }
 
 
 
 
 func main() {
-	m = make(chan bool 2)
-	var flag bool = false
+	m_1 := make(chan int)
+	m_2 := make(chan int)
+	timeout := make(chan bool)
 
-	fmt.Println("Alo mundo!")
+	go ana(m_1)
+	go maria(m_2)
+
+	msg_1 := <-m_1
+	msg_2 := <-m_2
+	
+	if msg_1 == 0 && msg_2 == 0 {
+		
+		go func() {
+			fmt.Println("Alarme foi acionado")
+			fmt.Println("Alarme está em contagem regressiva")
+			time.Sleep(6 * time.Second)
+			timeout <- true
+		}()
+	}
+
+	msg_1 = <-m_1
+	msg_2 = <-m_2
+
+	if msg_1 == 1 && msg_2 == 1 {
+		fmt.Println("Ana e Maria saíram e trancaram a porta")
+	}
+
+	select {
+		case <-timeout:	
+			fmt.Println("Alarme foi ativado")
+	}
 }
